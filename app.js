8ef528a69de3c656c89073eaa6055b9876273663
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-20 23:58:31 +0300

    update main app file

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
+
+const scannersRouter = require('./controllers/scanners')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 mongoose.set('strictQuery', false)
 
 mongoose.connect(config.MONGO_URI)
@@ -17,7 +22,9 @@ app.use(cors())
 
 app.use(middleware.requestLogger)
 
-
+app.use('/api/scanners', scannersRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpointHandler)
 app.use(middleware.errorHandler)
